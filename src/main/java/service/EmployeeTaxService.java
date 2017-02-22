7f3b392c529de4e0cd32da86c3c33f287f38c7b6
Senthil G <gdr.senthil@gmail.com>
2017-02-22 12:49:04 +0530

    Added FindAll and FindByEmpId in EmployeeTax DeclarationDAO and Service and test Functionality"

--- src/main/java/service/EmployeeTaxService.java
+++ src/main/java/service/EmployeeTaxService.java
@@ -1,16 +1,26 @@
 package service;
 
+import java.util.List;
+
 import dao.EmployeeTaxDeclarationDAO;
 import model.TaxDeclaration;
 
 public class EmployeeTaxService {
 
 	private EmployeeTaxDeclarationDAO employeeTaxDAO = new EmployeeTaxDeclarationDAO();
-	
+
 	public void register(TaxDeclaration empTax) {
 
 		employeeTaxDAO.applyTaxDeclaration(empTax);
 	}
-	
-	
+
+	public TaxDeclaration findByEmpId(Long empId) {
+
+		return employeeTaxDAO.findByEmpId(empId);
+	}
+
+	public List<TaxDeclaration> findAll() {
+
+		return employeeTaxDAO.findAll();
+	}
 }
--- src/test/java/dao/TestTaxDecalration.java
+++ src/test/java/dao/TestTaxDecalration.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.util.List;
+
 import model.Employee;
 import model.TaxDeclaration;
 
@@ -13,37 +15,46 @@ public class TestTaxDecalration {
 
 		td.setEmployee(emp);
 		td.setPanNo("ANYPG123L");
-		td.setHouseRent(10000.0f);
+		td.setHouseRent(10000);
 		td.setHouseOwnerName("RajaRathinam");
-		td.setMedicalBills(10000.0f);
-		td.setEpfvpfContribution(10000.0f);
-		td.setPpf(10000.0f);
-		td.setScss(10000.0f);
-		td.setNsc(10000.0f);
-		td.setTaxSavingFD(10000.0f);
-		td.setTaxSavingBonds(10000.0f);
-		td.setTaxSavingMutualFund(10000.0f);
-		td.setLifeInsurancePremiums(10000.0f);
-		td.setPensionPlan(10000.0f);
-		td.setCentralGovtPlan(10000.0f);
-		td.setHousingLoan(10000.0f);
-		td.setSukanyaSamriddhiAccount(10000.0f);
-		td.setStampDutyCharges(10000.0f);
-		td.setTuitionFees(10000.0f);
-		td.setAdditionalDeduction(10000.0f);
-		td.setDeductionRgess(10000.0f);
-		td.setMedicalInsuranceSelf(10000.0f);
-		td.setMedicalInsuranceParents(10000.0f);
-		td.setEducationLoan(10000.0f);
-		td.setMedicalTreatment(10000.0f);
-		td.setExpenditureMedicalTreatment(10000.0f);
-		td.setDonationApprovedFunds(10000.0f);
-		td.setPhysicallyDisabled(10000.0f);
-	
+		td.setMedicalBills(10000);
+		td.setEpfvpfContribution(10000);
+		td.setPpf(10000);
+		td.setScss(10000);
+		td.setNsc(10000);
+		td.setTaxSavingFD(10000);
+		td.setTaxSavingBonds(10000);
+		td.setTaxSavingMutualFund(10000);
+		td.setLifeInsurancePremiums(10000);
+		td.setPensionPlan(10000);
+		td.setCentralGovtPlan(10000);
+		td.setHousingLoan(10000);
+		td.setSukanyaSamriddhiAccount(10000);
+		td.setStampDutyCharges(10000);
+		td.setTuitionFees(10000);
+		td.setAdditionalDeduction(10000);
+		td.setDeductionRgess(10000);
+		td.setMedicalInsuranceSelf(10000);
+		td.setMedicalInsuranceParents(10000);
+		td.setEducationLoan(10000);
+		td.setMedicalTreatment(10000);
+		td.setExpenditureMedicalTreatment(10000);
+		td.setDonationApprovedFunds(10000);
+		td.setPhysicallyDisabled(10000);
 
 		EmployeeTaxDeclarationDAO dao = new EmployeeTaxDeclarationDAO();
-		dao.applyTaxDeclaration(td);
-		System.out.println("Insertd"+td);
+		// dao.applyTaxDeclaration(td);
+		System.out.println("Insertd" + td);
+
+		/*
+		 * TaxDeclaration findByEmpId = dao.findByEmpId(1L);
+		 * System.out.println(findByEmpId);
+		 */
+
+		List<TaxDeclaration> list = dao.findAll();
+		for (TaxDeclaration taxDeclaration : list) {
+			System.out.println(taxDeclaration);
+		}
 	}
 
 }
