4aefd8c465c577685846bb206e17fe518f1f80e6
Senthil G <gdr.senthil@gmail.com>
2017-02-23 13:49:01 +0530

    "Added Employee Salary Service in Service Functionality"

---
+++ src/main/java/service/EmployeeSalaryService.java
@@ -0,0 +1,27 @@
+package service;
+
+import java.util.List;
+
+import dao.EmployeeSalaryDAO;
+import model.EmployeeSalaryDetails;
+
+public class EmployeeSalaryService {
+
+	private EmployeeSalaryDAO employeeSalaryDAO = new EmployeeSalaryDAO();
+
+	public List<EmployeeSalaryDetails> list() {
+
+		return employeeSalaryDAO.list();
+	}
+
+	public List<EmployeeSalaryDetails> listMySalary(Long empId) {
+
+		return employeeSalaryDAO.listMySalary(empId);
+	}
+
+	public void insert(EmployeeSalaryDetails emp) {
+
+		employeeSalaryDAO.insert(emp);
+	}
+
+}
